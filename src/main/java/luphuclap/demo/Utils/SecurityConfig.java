e281d327aad8723c349dbc7e5befddacfb640af8
PhucLap123 <luphuclap@gmail.com>
2023-06-07 08:59:47 +0700

    nop bai tuan 6

--- src/main/java/luphuclap/demo/Utils/SecurityConfig.java
+++ src/main/java/luphuclap/demo/Utils/SecurityConfig.java
@@ -12,22 +12,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
 
+public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new CustomUserDetailService();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
@@ -35,32 +32,40 @@ public class SecurityConfig {
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers( "/css/**", "/js/**", "/", "/register", "/error")
+
                         .permitAll()
+
                         .requestMatchers( "/books/edit", "/books/delete")
+
                         .hasAnyAuthority("ADMIN")
+
                         .requestMatchers("/books", "/books/add")
-                        .hasAnyAuthority("ADMIN", "USER")
-                        .requestMatchers("/api/**")
-                        .hasAnyAuthority("ADMIN", "USER")
+
+                        .hasAnyAuthority("ADMIN","USER")
+
                         .anyRequest().authenticated()
+
                 )
                 .logout(logout -> logout.logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
                         .deleteCookies("JSESSIONID")
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
+
                         .permitAll()
+
                 )
                 .formLogin(formLogin -> formLogin.loginPage("/login")
                         .loginProcessingUrl("/login")
                         .defaultSuccessUrl("/")
+
                         .permitAll()
+
                 )
                 .rememberMe(rememberMe -> rememberMe.key("uniqueAndSecret")
                         .tokenValiditySeconds(86400)
--- src/main/java/luphuclap/demo/services/UserService.java
+++ src/main/java/luphuclap/demo/services/UserService.java
@@ -2,17 +2,27 @@ package luphuclap.demo.services;
 
 
 import luphuclap.demo.entity.User;
+import luphuclap.demo.repository.IRoleRepository;
 import luphuclap.demo.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class UserService {
+
     @Autowired
     private IUserRepository userRepository;
 
-    public void save (User user){
-        userRepository.save(user);
-    }
+    @Autowired
+    private IRoleRepository roleRepository;
 
+    public void save(User user){
+        userRepository.save(user);
+        Long userId = userRepository.getUserIdByUsername(user.getUsername());
+        Long roleId = roleRepository.getRoleIdByName("USER");
+        if (roleId != 0 && userId != 0){
+            userRepository.addRoleToUser(userId,roleId);
+        }
+    }
 }
